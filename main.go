851593cd3f6c26065345588a696f47c069cee616
Bill Rich <bill.rich@gmail.com>
2022-03-10 22:34:38 -0800

    starting on body parsing

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bytes"
 	"encoding/binary"
 	log "github.com/sirupsen/logrus"
 	"os"
@@ -11,14 +12,49 @@ import (
 
 type GeneralsHeader struct {
 	GameType       *ByteString `byte:"size=6"`
-	TimeStampBegin *ByteInt    `byte:"size=4"`
-	TimeStampEnd   *ByteInt    `byte:"size=4"`
-	Unknown        *ByteInt    `byte:"size=1"`
-	FileName       *ByteString `byte:"size=2,nullterm"`
+	TimeStampBegin *ByteInt32  `byte:"size=4"`
+	TimeStampEnd   *ByteInt32  `byte:"size=4"`
+	NumTimeStamps  *ByteInt16  `byte:"size=2"`
+	Unknown1       *ByteRaw    `byte:"size=12"`
+	FileName       *ByteString `byte:"size=2,nullterm,smallchar"`
+	Junk           *ByteRaw    `byte:"size=16"`
+	Version        *ByteString `byte:"size=2,nullterm,smallchar"`
+	DateTime       *ByteString `byte:"size=2,nullterm,smallchar"`
+	Junk2          *ByteRaw    `byte:"size=12"`
+	Map            *ByteString `byte:"size=2,nullterm"`
+	Unknown2       *ByteInt16  `byte:"size=2"`
+	Unknown3       *ByteInt32  `byte:"size=4"`
+	Unknown4       *ByteInt32  `byte:"size=4"`
+	Unknown5       *ByteInt32  `byte:"size=4"`
+	Unknown6       *ByteInt32  `byte:"size=4"`
+}
+
+type GeneralsBody struct {
+	TimeCode       *ByteInt32 `byte:"size=4"`
+	Order          *ByteInt32 `byte:"size=4"`
+	Number         *ByteInt32 `byte:"size=4"`
+	UniqueArgCount *ByteRaw   `byte:"size=1"`
+	Orders
+}
+
+type GeneralsOrder struct {
+	OrderType    *ByteRaw `byte:"size=1"`
+	NumberOfArgs *ByteRaw `byte:"size=1"`
 }
 
 type ByteInterface interface {
 	Write([]byte)
+}
+
+type ByteRaw []byte
+
+func NewByteRaw(b []byte) *ByteRaw {
+	br := ByteRaw(b)
+	return &br
+}
+
+func (br *ByteRaw) Write(b []byte) {
+	*br = ByteRaw(b)
 }
 
 type ByteString string
@@ -32,10 +68,26 @@ func (bs *ByteString) Write(b []byte) {
 	*bs = ByteString(b)
 }
 
-type ByteInt int64
+type ByteInt32 int32
 
-func (bs *ByteInt) Write(b []byte) {
-	*bs = ByteInt(binary.BigEndian.Uint32(b))
+func NewByteInt32(i int32) *ByteInt32 {
+	bs := ByteInt32(i)
+	return &bs
+}
+
+func (bs *ByteInt32) Write(b []byte) {
+	*bs = ByteInt32(binary.LittleEndian.Uint32(b))
+}
+
+type ByteInt16 int16
+
+func NewByteInt16(i int16) *ByteInt16 {
+	bs := ByteInt16(i)
+	return &bs
+}
+
+func (bs *ByteInt16) Write(b []byte) {
+	*bs = ByteInt16(binary.LittleEndian.Uint16(b))
 }
 
 func main() {
@@ -44,7 +96,22 @@ func main() {
 		log.WithError(err).Fatal("could not open file")
 	}
 	header := GeneralsHeader{
-		GameType: NewByteString(""),
+		GameType:       NewByteString(""),
+		TimeStampBegin: NewByteInt32(0),
+		TimeStampEnd:   NewByteInt32(0),
+		NumTimeStamps:  NewByteInt16(0),
+		Unknown1:       NewByteRaw([]byte{}),
+		FileName:       NewByteString(""),
+		Junk:           NewByteRaw([]byte{}),
+		Version:        NewByteString(""),
+		DateTime:       NewByteString(""),
+		Junk2:          NewByteRaw([]byte{}),
+		Map:            NewByteString(""),
+		Unknown2:       NewByteInt16(0),
+		Unknown3:       NewByteInt32(0),
+		Unknown4:       NewByteInt32(0),
+		Unknown5:       NewByteInt32(0),
+		Unknown6:       NewByteInt32(0),
 	}
 	inst := reflect.ValueOf(&header).Elem()
 	gtype := reflect.TypeOf(header)
@@ -57,6 +124,7 @@ func main() {
 		}
 		//fieldValue := []byte{}
 		_, nullterm := tagKv["nullterm"]
+		_, smallchar := tagKv["smallchar"]
 		switch {
 		case !nullterm:
 			fieldRaw := make([]byte, size)
@@ -74,10 +142,34 @@ func main() {
 			field.Write(fieldRaw)
 
 		case nullterm:
+			fieldRaw := bytes.Buffer{}
+			for {
+				b := make([]byte, size)
+				sizeRead, err := file.Read(b)
+				if sizeRead != size {
+					log.Errorf("unable to read fieldRaw. expected size: %d, got: %d", size, sizeRead)
+					break
+				}
 
+				if err != nil {
+					log.WithError(err).Error("unable to read fieldRaw")
+				}
+				if b[0] == byte(0) && b[1] == byte(0) {
+					break
+				}
+				switch {
+				case smallchar:
+					fieldRaw.Write(b[:1])
+				case !smallchar:
+					fieldRaw.Write(b)
+				}
+			}
+			field := inst.Field(i).Interface().(ByteInterface)
+
+			field.Write(fieldRaw.Bytes())
 		}
 	}
-	log.Printf("%+v", header)
+	log.Printf("%+v", *header.Map)
 }
 
 func parseTag(rawTag string) map[string]string {
